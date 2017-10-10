67a5d9f5bfbdf2d98ac8efe149e59c46654fe0fe
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2017-10-10 13:39:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ function setupElements(){
 	picElements=[];
 	for(i=0;i<3;i++){
 		picElements.push(new Object());
-		picElements[picElements.length-1].set($CANV.width,$CANV.height/3.5,i,$CANV.width/3.82,$CANV.width/3.82);
+		picElements[picElements.length-1].set($CANV.width,$CANV.height/3.5,i,$CANV.width/2.82,$CANV.width/2.82);
 	}
 }
 
